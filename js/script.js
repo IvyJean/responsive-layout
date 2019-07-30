9bf6e7311e6b0e4953445bf382e700fae50113ab
IVY_JEAN <ivy.cocharo@whitecloack.com>
2019-07-30 13:36:40 +0800

    can now be stored to local

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-
-const form = document.querySelector('form')
-const ul = document.querySelector('ul');
-const button = document.querySelector('button')
-const title = document.getElementById('title')
-const content = document.getElementById('content')
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
-
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const button = document.querySelector("button");
+// const title = document.getElementById("title");
+// const content = document.getElementById("content");
+// let itemsArray = localStorage.getItem("items")
+//   ? JSON.parse(localStorage.getItem("items"))
+//   : [];
 
 // a conditional statement that checks if localStorage already exists
 // if (localStorage.getItem('items')) {
@@ -14,18 +14,36 @@ let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem
 //     items = []
 // }
 
-button.addEventListener('click', function(e) {
-    e.preventDefault()
+let articles = [];
 
-    itemsArray.push(title.value)
-    itemsArray.push(content.value)
-    localStorage.setItem('itemsArray', JSON.stringify(itemsArray))
-    console.log(content.value);
-  })
+console.log(document.getElementById("content".value));
 
-  localStorage.setItem('itemsArray', JSON.stringify(itemsArray))
-  const data = JSON.parse(localStorage.getItem('items'))
+const addArticle = e => {
+  e.preventDefault();
+  let article = {
+    id: Date(),
+    title: document.getElementById("title").value,
+    content: document.getElementById("content-text").value
+  };
 
-  
-  
+  articles.push(article);
+  console.log(articles);
 
+  localStorage.setItem("itemArray", JSON.stringify(articles));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btn").addEventListener("click", addArticle);
+});
+
+// button.addEventListener('click', function(e) {
+//     e.preventDefault()
+
+//     itemsArray.push(title.value)
+//     itemsArray.push(content.value)
+//     localStorage.setItem('items', JSON.stringify(itemsArray))
+//     console.log(itemsArray);
+//   })
+
+//   localStorage.setItem('items', JSON.stringify(itemsArray))
+//   const data = JSON.parse(localStorage.getItem('items'))
