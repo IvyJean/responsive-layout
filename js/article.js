ad7f426f80eaee3c7c23aee72bc23dbdd4eb1a90
IVY_JEAN <ivy.cocharo@whitecloack.com>
2019-07-31 13:23:32 +0800

    working article

--- js/article.js
+++ js/article.js
@@ -1,63 +1,94 @@
-var primary_id = 0;
-var form_content = document.getElementById("content");
-var btn_save = document.getElementById("btn-save");
+// post button
+const post = document.querySelector("#submit");
 
-var articleList;
+// article stuff
+const titleElement = document.querySelector("#title");
+const bodyElement = document.querySelector("#body");
 
-// init js
-function initialize() {
-  if (!!window.localStorage.getItem("articleList")) {
-    articleList = JSON.parse(window.localStorage.getItem("articleList"));
-  } else {
-    articleList = [];
-  }
-  btn_save.addEventListener("click", saveObject);
-  console.log("INIT!");
-  showList();
+// var options = {
+//   weekday: "long",
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+//   hour: "numeric",
+//   minute: "numeric"
+// };
+
+var article_object;
+if (localStorage.getItem("article")) {
+  article_object = JSON.parse(localStorage.getItem("article"));
+} else {
+  article_object = [];
 }
 
-//crud stuff
-
-function showList() {
-  if (!!articleList.length) {
-    getLastTaskId();
-  }
-}
-
-function showArticles() {
-  if (!!articleList.length) {
-    getLastTaskId();
-    for (var item in articleList) {
-      var article_object = articleList[item];
-      addTaskToList(task);
-    }
-    syncEvents();
-  }
-}
-
-function saveObject(event) {
-  var articleObject = {
-    id: primary_id,
-    title: document.getElementById("article-title").value,
-    timestamp: new Date().toLocaleString(),
-    content: document.getElementById("article-content").value
+function article() {
+  var time = new Date().toLocaleString();
+  var aObject = {
+    title: titleElement.value,
+    body: bodyElement.value,
+    time: time
   };
-  articleList.push(articleObject);
-  setArticle();
-  primary_id++;
+
+  article_object.push(aObject);
+  localStorage.setItem("article", JSON.stringify(article_object));
 }
 
-//common
+post.addEventListener("click", article);
 
-function getLastTaskId() {
-  var lastArticle = articleList[articleList.length - 1];
-  primary_id = lastArticle.id + 1;
-}
 
-function setArticle() {
-  console.log("SAVED!!");
-  window.localStorage.setItem("articleList", JSON.stringify(articleList));
-  articleList = JSON.parse(window.localStorage.getItem("articleList"));
-}
 
-initialize();
+// var id = 0;
+// var form_content = document.getElementById("content");
+// var btn_save = document.getElementById("btn-save");
+
+// var articleList;
+
+
+  // if (!!window.localStorage.getItem("articleList")) {
+  //   articleList = JSON.parse(window.localStorage.getItem("articleList"));
+  // } else {
+  //   articleList = [];
+  // }
+  // btn_save.addEventListener("click", saveObject);
+  // showList();
+
+// function showList() {
+//   if (!!articleList.length) {
+//     getLastTaskId();
+//   }
+// }
+
+// function showArticles() {
+//   if (!!articleList.length) {
+//     getLastTaskId();
+//     for (var item in articleList) {
+//       var article_object = articleList[item];
+//       addTaskToList(task);
+//     }
+//     syncEvents();
+//   }
+// }
+
+// function saveObject(event) {
+//   var articleObject = {
+//     id: primary_id,
+//     title: document.getElementById("article-title").value,
+//     timestamp: new Date().toLocaleString(),
+//     content: document.getElementById("article-content").value
+//   };
+//   articleList.push(articleObject);
+//   setArticle();
+//   primary_id++;
+// }
+
+// function getLastTaskId() {
+//   var lastArticle = articleList[articleList.length - 1];
+//   primary_id = lastArticle.id + 1;
+// }
+
+// function setArticle() {
+//   window.localStorage.setItem("articleList", JSON.stringify(articleList));
+//   articleList = JSON.parse(window.localStorage.getItem("articleList"));
+// }
+
+
--- js/script.js
+++ js/script.js
@@ -1,32 +1,79 @@
-// let template = document.getElementById("template");
-// let articlesTitle = document.getElementById("article-text");
-// let articleWrapper = template.content.querySelector("article");
-// let title = articleWrapper.querySelector("#article-title");
-// let timestamp = articleWrapper.querySelector("#article-timestamp");
-// let content = articleWrapper.querySelector("#article-content");
+
+var article_object = JSON.parse(localStorage.getItem("article"));
+
+var article_object_copy = article_object.slice(0).reverse();
+
+var temp = document.querySelector("#template");
+var item = temp.content.querySelector("#bodyContainer");
+
+var i;
+for (i = 0; i < article_object_copy.length; i++) {
+  var imp = document.importNode(item, true);
+
+  var item2 = imp.querySelector("#article");
+  var title = item2.querySelector("#title");
+  var subtitle = item2.querySelector("#subtitle");
+  var body = item2.querySelector("#body");
+  var deleteButton = item2.querySelector("#delete");
+
+  var button = document.createElement("button");
+  button.setAttribute("id", i);
+  button.textContent = "Delete Article";
+  deleteButton.appendChild(button);
+
+  function deleteArticle(e) {
+    var removeArticle = e.target.parentNode.parentNode.parentNode;
+    document.getElementById("stream").removeChild(removeArticle);
+
+    article_object_copy.splice(e.target.id, 1);
+    localStorage.setItem("article", JSON.stringify(article_object_copy));
+
+    location.reload();
+  }
+
+  button.addEventListener("click", deleteArticle);
+
+  title.innerHTML = article_object_copy[i].title;
+  subtitle.innerHTML = article_object_copy[i].time;
+  body.innerHTML = article_object_copy[i].body;
+  imp.setAttribute("id", "article" + i);
+  document.getElementsByClassName("stream")[0].appendChild(imp);
+}
+
+// function delete() {
+
+// }
+
+// let temp = document.getElementById("template");
+// let articlesTitle = document.getElementById("articleNotif");
+// let container = template.content.querySelector("article");
+// let title = container.querySelector("articleTitle");
+// let timestamp = container.querySelector("#articleTime");
+// let content = container.querySelector("#article-content");
+// let id = 0;
+// let articleList;
 
 
-// let articles = [];
+// if (!!window.localStorage.getItem("articleList")) {
+//   articleList = JSON.parse(window.localStorage.getItem("articleList"));
+//   } else {
+//     articlesTitle.innerHTML = "<center>No Articles</center>";
+//     articleList = [];
+//   }
+//   showList();
 
-// console.log(document.getElementById("content".value));
-
-// const addArticle = e => {
-//   e.preventDefault();
-//   let article = {
-//     id: Date(),
-//     title: document.getElementById("title").value,
-//     content: document.getElementById("content-text").value
-//   };
-
-//   articles.push(article);
-//   console.log(articles);
-
-//   localStorage.setItem("itemArray", JSON.stringify(articles));
-// };
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.getElementById("btn").addEventListener("click", addArticle);
-// });
+// function showList() {
+//   if (!!articleList.length) {
+//     getLastTaskId();
+//     for (var item in articleList) {
+//       var articles = articleList[item];
+//       displayList(articles);
+//       var imp = document.importNode(container, true);
+//       document.getElementById("main").appendChild(imp);
+//       console.log(articles);
+//     }
+//   }
+// }
 
 // function displayList(articles) {
 //   title.innerHTML = articles.title;
@@ -36,58 +83,14 @@
 //   console.log("ID:", btn_delete.getAttribute("id"));
 // }
 
-var template = document.getElementById("template");
-var articlesTitle = document.getElementById("article-text");
-var articleWrapper = template.content.querySelector("article");
-var title = articleWrapper.querySelector("#article-title");
-var timestamp = articleWrapper.querySelector("#article-timestamp");
-var content = articleWrapper.querySelector("#article-content");
-var btn_delete = articleWrapper.querySelector("#btn-delete");
-var primary_id = 0;
-var articleList;
+// function removeTask(e) {
+//   console.log("Hello");
+// }
+
+// function getLastTaskId() {
+//   var lastArticle = articleList[articleList.length - 1];
+//   id = lastArticle.id + 1;
+//   console.log("LAST ID: ", id);
+// }
 
 
-if (!!window.localStorage.getItem("articleList")) {
-  articleList = JSON.parse(window.localStorage.getItem("articleList"));
-  } else {
-    console.log("EMPTY");
-    articlesTitle.innerHTML = "<center>No Articles</center>";
-    article_List = [];
-  }
-  showList();
-
-
-// crud
-function showList() {
-  if (!!article_List.length) {
-    getLastTaskId();
-    for (var item in article_List) {
-      var articles = article_List[item];
-      displayList(articles);
-      var imp = document.importNode(articleWrapper, true);
-      document.getElementById("main").appendChild(imp);
-      console.log(articles);
-    }
-  }
-}
-
-function displayList(articles) {
-  title.innerHTML = articles.title;
-  timestamp.innerHTML = articles.timestamp;
-  content.innerHTML = articles.content;
-  btn_delete.setAttribute("id", articles.id);
-  console.log("ID:", btn_delete.getAttribute("id"));
-}
-
-function removeTask(e) {
-  console.log("Hello");
-}
-
-//common
-function getLastTaskId() {
-  var lastArticle = article_List[article_List.length - 1];
-  primary_id = lastArticle.id + 1;
-  console.log("LAST ID: ", primary_id);
-}
-
-init();
